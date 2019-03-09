059fb541492d99222baff27771b925f2ba8f4420
mohawow <moha.almahbashi@gmail.com>
2019-03-09 00:26:40 -0800

    changed demo user in seed file

--- seed.js
+++ seed.js
@@ -8,33 +8,33 @@ const data = [
   {
     name: "Morning",
     trips: [
-      { block: "8:30AM - 9:30AM", date: moment().toJSON(), numberOfStops: 2, numberOfPackages: 3, initialPay: 35, finalPay: 40, tips: 5, userId: "5c836d057c72871f70e81ca5" },
-      { block: "8:30AM - 9:30AM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 35, finalPay: 40, tips: 5, userId: "5c836d057c72871f70e81ca5" },
-      { block: "8:30AM - 9:30AM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 35, finalPay: 40, tips: 5, userId: "5c836d057c72871f70e81ca5" }
+      { block: "8:30AM - 9:30AM", date: moment().toJSON(), numberOfStops: 2, numberOfPackages: 3, initialPay: 35, finalPay: 40, tips: 5, userId:"5c75e2fa8ba203e53b18ea0a" },
+      { block: "8:30AM - 9:30AM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 35, finalPay: 40, tips: 5, userId:"5c75e2fa8ba203e53b18ea0a" },
+      { block: "8:30AM - 9:30AM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 35, finalPay: 40, tips: 5, userId:"5c75e2fa8ba203e53b18ea0a" }
     ]
   },
   {
     name: "Afternoon",
     trips: [
-      { block: "12:30PM - 01:30PM", date: moment().toJSON(), numberOfPackages: 5, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId: "5c836d057c72871f70e81ca5" },
-      { block: "12:30PM - 01:30PM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId: "5c836d057c72871f70e81ca5"},
-      { block: "12:30PM - 01:30PM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId: "5c836d057c72871f70e81ca5" }
+      { block: "12:30PM - 01:30PM", date: moment().toJSON(), numberOfPackages: 5, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId:"5c75e2fa8ba203e53b18ea0a" },
+      { block: "12:30PM - 01:30PM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId:"5c75e2fa8ba203e53b18ea0a"},
+      { block: "12:30PM - 01:30PM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId:"5c75e2fa8ba203e53b18ea0a" }
     ]
   },
   {
     name: "Evening",
     trips: [
-      { block: "04:30PM - 06:30PM", date:moment().toJSON(), numberOfPackages: 5, numberOfStops: 2, initialPay: 25,finalPay: 30, tips: 10, userId: "5c836d057c72871f70e81ca5"  },
-      { block: "04:30PM - 06:30PM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId: "5c836d057c72871f70e81ca5" },
-      { block: "04:30PM - 06:30PM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId: "5c836d057c72871f70e81ca5"  }
+      { block: "04:30PM - 06:30PM", date:moment().toJSON(), numberOfPackages: 5, numberOfStops: 2, initialPay: 25,finalPay: 30, tips: 10, userId:"5c75e2fa8ba203e53b18ea0a"  },
+      { block: "04:30PM - 06:30PM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId:"5c75e2fa8ba203e53b18ea0a" },
+      { block: "04:30PM - 06:30PM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 25, finalPay: 30, tips: 10, userId:"5c75e2fa8ba203e53b18ea0a"  }
     ]
   },
   {
     name: "Night",
     trips: [
-      { block: "07:30PM - 08:30PM", date: moment().toJSON(), numberOfPackages: 5, numberOfStops: 2, initialPay: 45, finalPay: 50, tips: 5, userId: "5c836d057c72871f70e81ca5"},
-      { block: "07:30PM - 08:30PM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 45, finalPay: 50, tips: 5, userId: "5c836d057c72871f70e81ca5" },
-      { block: "07:30PM - 08:30PM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 45, finalPay: 50, tips: 5, userId: "5c836d057c72871f70e81ca5" }
+      { block: "07:30PM - 08:30PM", date: moment().toJSON(), numberOfPackages: 5, numberOfStops: 2, initialPay: 45, finalPay: 50, tips: 5, userId:"5c75e2fa8ba203e53b18ea0a"},
+      { block: "07:30PM - 08:30PM", date: moment().toJSON(), numberOfPackages: 10, numberOfStops: 2, initialPay: 45, finalPay: 50, tips: 5, userId:"5c75e2fa8ba203e53b18ea0a" },
+      { block: "07:30PM - 08:30PM", date: moment().toJSON(), numberOfPackages: 15, numberOfStops: 2, initialPay: 45, finalPay: 50, tips: 5, userId:"5c75e2fa8ba203e53b18ea0a" }
     ]
   }
 ];
